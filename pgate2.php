7f7bdac87847453b86793e289a139c0c9f5d29bf
mores1 <97749252+mores1@users.noreply.github.com>
2022-06-22 20:03:39 +0900

    06221

---
+++ pgate2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    $name = 'にんじゃわんこ';
+    // 'こんにちは！'という文字列と$nameを連結してechoしてください
+    
+    echo 'こんにちは!'.$name;
+    
+  ?>
+
+</body>
+</html>
