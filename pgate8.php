cf63cf858c0abd88a524f454867ff3ae93231a2a
mores1 <97749252+mores1@users.noreply.github.com>
2022-06-23 22:35:21 +0900

    3

---
+++ pgate8.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 変数$iを定義してください
+    $i = 2;
+    
+    // while文を書いてください
+    while ($i <= 100){
+      if ( $i % 2 == 0){
+        echo $i;
+        echo '<br>';
+      }
+      $i ++;
+    }
+    
+  ?>
+
+</body>
+</html>
+
